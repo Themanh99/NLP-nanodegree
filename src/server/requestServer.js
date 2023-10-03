d802c84aacde1cb2663947e5a63b7a5e63186e32
Themanh99 <themanhchu99@gmail.com>
2023-10-04 01:09:12 +0700

    add call API

--- src/server/requestServer.js
+++ src/server/requestServer.js
@@ -12,6 +12,7 @@ const checkRequest = async (req, res, next) => {
   }
 
   let API_KEY = null;
+  let API_URL = process.env.API_URL;
   console.log("Call API.....");
   try {
     API_KEY = process.env.API_KEY;
@@ -25,6 +26,25 @@ const checkRequest = async (req, res, next) => {
   } catch (err) {
     return next(err);
   }
+
+  const formdata = new FormData();
+  formdata.append("key", API_KEY);
+  formdata.append("txt", "YOUR TEXT HERE");
+  formdata.append("lang", "vi");
+
+  const requestOptions = {
+    method: "POST",
+    body: formdata,
+    redirect: "follow",
+  };
+
+  const response = fetch(API_URL, requestOptions)
+    .then((response) => ({
+      status: response.status,
+      body: response.json(),
+    }))
+    .then(({ status, body }) => console.log(status, body))
+    .catch((error) => console.log("error", error));
 };
 
 export { checkRequest };
